ab0966f72e55f957e553b7d19ce2fecbbd4b8d00
uvuso <vuse24021995>
2020-06-11 17:54:07 +0300

    Replace @JsonFormat annotation to @DateTimeFormat in get method for MovieSessionController

--- src/main/java/cinema/controller/MovieSessionController.java
+++ src/main/java/cinema/controller/MovieSessionController.java
@@ -10,10 +10,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,9 +28,9 @@ public class MovieSessionController {
 
     @GetMapping("/available")
     public List<MovieSessionResponseDto> getAll(
-            Long movieId,
-            @JsonFormat(shape = JsonFormat.Shape.STRING,
-                    pattern = "dd.MM.yyyy") LocalDateTime date) {
+            @RequestParam(required = false) Long movieId,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date) {
         return movieSessionService.findAvailableSessions(movieId, date)
                 .stream()
                 .map(m -> movieSessionMaper.transfer(m))
