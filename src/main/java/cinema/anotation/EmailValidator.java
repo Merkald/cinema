ded36a690dff9ad6ecac22f68141996958a670d3
uvuso <vuse24021995>
2020-06-15 11:30:45 +0300

    remove obvoius operator

--- src/main/java/cinema/anotation/EmailValidator.java
+++ src/main/java/cinema/anotation/EmailValidator.java
@@ -9,6 +9,6 @@ public class EmailValidator implements ConstraintValidator<EmailConstraint, Stri
     public boolean isValid(String email, ConstraintValidatorContext context) {
         String regex = ".+@.+\\..+";
         return email != null && email.matches(regex)
-                && email.length() > 8 && email.length() < 30;
+                && email.length() > 8;
     }
 }
